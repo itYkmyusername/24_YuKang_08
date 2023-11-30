286ce938974ad7fb088aa3a82d95522a341bc543
A-Cute-Loli <146807353+A-Cute-Loli@users.noreply.github.com>
2023-12-01 03:03:48 +0800

    Updated Unity Project by Kenneth

--- 24_YuKang_GitHub08/Assets/Scripts/CalculatorScript.cs
+++ 24_YuKang_GitHub08/Assets/Scripts/CalculatorScript.cs
@@ -7,8 +7,16 @@ public class CalculatorScript : MonoBehaviour
 {
     private float SGDUSD = 0.74f;
     private float SGDJPY = 82.78f;
+    private float SGDRM = 3.08f;
+    private float SGDEUR = 0.63f;
+    private float SGDKRW = 881.54f;
+    private float SGDTWD = 20.73f;
     public Toggle toggleUSDollar;
     public Toggle toggleJPYen;
+    public Toggle toggleMalaysianRM;
+    public Toggle toggleEuropeanEUR;
+    public Toggle toggleKoreanKRW;
+    public Toggle toggleTaiwanTWD;
     public InputField inputConvertedAmount;
     public InputField inputAmount;
     public Text DebugText;
@@ -19,17 +27,37 @@ public class CalculatorScript : MonoBehaviour
         {
             float amount = float.Parse(inputAmount.text);
 
-            if (toggleUSDollar.isOn == true && toggleJPYen.isOn == false)
+            if (toggleUSDollar.isOn == true && toggleJPYen.isOn == false && toggleMalaysianRM.isOn == false && toggleEuropeanEUR.isOn == false && toggleKoreanKRW.isOn == false && toggleTaiwanTWD.isOn == false)
             {
                 amount *= SGDUSD;
                 inputConvertedAmount.text = "$ " + (amount);
             }
-            else if (toggleJPYen.isOn == true && toggleUSDollar.isOn == false)
+            else if (toggleJPYen.isOn == true && toggleUSDollar.isOn == false && toggleMalaysianRM.isOn == false && toggleEuropeanEUR.isOn == false && toggleKoreanKRW.isOn == false && toggleTaiwanTWD.isOn == false)
             {
                 amount *= SGDJPY;
                 inputConvertedAmount.text = "$ " + (amount);
             }
-            else if (toggleJPYen.isOn == false && toggleUSDollar.isOn == false)
+            else if (toggleMalaysianRM.isOn == true && toggleJPYen.isOn == false && toggleUSDollar.isOn == false && toggleEuropeanEUR.isOn == false && toggleKoreanKRW.isOn == false && toggleTaiwanTWD.isOn == false)
+            {
+                amount *= SGDRM;
+                inputConvertedAmount.text = "$ " + (amount);
+            }
+            else if (toggleEuropeanEUR.isOn == true && toggleJPYen.isOn == false && toggleUSDollar.isOn == false && toggleMalaysianRM.isOn == false && toggleKoreanKRW.isOn == false && toggleTaiwanTWD.isOn == false)
+            {
+                amount *= SGDEUR;
+                inputConvertedAmount.text = "$ " + (amount);
+            }
+            else if(toggleKoreanKRW.isOn == true && toggleJPYen.isOn == false && toggleUSDollar.isOn == false && toggleMalaysianRM.isOn == false && toggleEuropeanEUR.isOn == false && toggleTaiwanTWD.isOn == false)
+            {
+                amount *= SGDKRW;
+                inputConvertedAmount.text = "$ " + (amount);
+            }
+            else if(toggleTaiwanTWD.isOn == true && toggleJPYen.isOn == false && toggleUSDollar.isOn == false && toggleMalaysianRM.isOn == false && toggleEuropeanEUR.isOn == false && toggleKoreanKRW.isOn == false)
+            {
+                amount *= SGDTWD;
+                inputConvertedAmount.text = "$ " + (amount);
+            }
+            else if (toggleJPYen.isOn == false && toggleUSDollar.isOn == false && toggleMalaysianRM.isOn == false && toggleEuropeanEUR.isOn == false && toggleKoreanKRW.isOn == false && toggleTaiwanTWD.isOn == false)
             {
                 inputConvertedAmount.text = "$ " + (amount);
             }
@@ -50,6 +78,10 @@ public class CalculatorScript : MonoBehaviour
         inputAmount.text = "";
         toggleUSDollar.isOn = false;
         toggleJPYen.isOn = false;
+        toggleMalaysianRM.isOn = false;
+        toggleEuropeanEUR.isOn = false;
+        toggleKoreanKRW.isOn = false;
+        toggleTaiwanTWD.isOn = false;
         inputConvertedAmount.text = "";
     }
 
